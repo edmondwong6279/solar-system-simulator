e33aa84174f440162a4a96804b04f0bb2be2b8a0
edmondwong6279 <edmondwong6279@gmail.com>
2022-11-03 01:35:12 +0000

    moved data directly into api

--- src/pages/api/getData.ts
+++ src/pages/api/getData.ts
@@ -1,57 +1,37 @@
-/* eslint-disable no-console */
-import * as fs from 'fs';
 import { NextApiRequest, NextApiResponse } from 'next';
 
+const dataVelocity = [47900, 35000, 29800, 24100, 13100, 9700, 6800, 5400, 0];
+
+const dataDistance = [
+	57900000, 108200000, 149600000, 228000000, 778500000, 1432000000, 2867000000, 4515000000, 0,
+];
+
+const dataDiameter = [4879, 12104, 12756, 6792, 142984, 120536, 51118, 49528, 1392700];
+
+const dataMass = [0.33, 4.87, 5.97, 0.642, 1898, 568, 86.8, 102, 1989000];
+
+const dataNames = [
+	'mercury',
+	'venus',
+	'earth',
+	'mars',
+	'jupiter',
+	'saturn',
+	'uranus',
+	'neptune',
+	'sun',
+];
+
 const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
-	console.log('Insider handler');
-	console.log(process.cwd());
-	console.log(__filename);
-	console.log(process.env.PUBLIC_URL);
-
-	// read line by line, process from these coords to xyz coords, push into an array
-	fs.readFile(`/data.csv`, 'utf8', (err, data) => {
-		if (err) {
-			console.error(err);
-			return;
-		}
-
-		const processedMass: number[] = [];
-		const processedDiamter: number[] = [];
-		const processedDistance: number[] = [];
-		const processedVelocity: number[] = [];
-		const processedNames: string[] = [];
-		const lines = data.split('\n');
-
-		lines.forEach((line, idx) => {
-			if (idx !== 0) {
-				const current = line.split(',');
-				const [name, mass, diameter, distance, velocity] = [
-					current[0],
-					Number(current[1]),
-					Number(current[2]),
-					Number(current[3]),
-					Number(current[4]),
-				];
-
-				processedMass.push(mass);
-				processedDiamter.push(diameter);
-				processedDistance.push(distance);
-				processedVelocity.push(velocity);
-				processedNames.push(name);
-			}
-		});
-
-		console.log(`Getting ${processedNames.length} bodies.`);
-		res.send(
-			JSON.stringify({
-				names: processedNames,
-				distance: processedDistance,
-				mass: processedMass,
-				diameter: processedDiamter,
-				velocity: processedVelocity,
-			})
-		);
-	});
+	res.send(
+		JSON.stringify({
+			names: dataNames,
+			distance: dataDistance,
+			mass: dataMass,
+			diameter: dataDiameter,
+			velocity: dataVelocity,
+		})
+	);
 };
 
 export default handler;
